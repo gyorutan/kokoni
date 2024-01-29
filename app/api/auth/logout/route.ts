348613c59de9a69a81745af865d9e98c60fc0c18
gyorutan <waou1998@gmail.com>
2024-01-29 09:54:21 +0900

    commit

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,16 +1,13 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export const GET = () => {
+export const GET = (req: NextRequest) => {
   try {
     const response = NextResponse.json({
       success: true,
       message: "유저 로그아웃 성공",
     });
 
-    response.cookies.set("token", "", {
-      httpOnly: true,
-      expires: new Date(0),
-    });
+    response.cookies.set("token", "", { httpOnly: true });
 
     return response;
   } catch (error) {
