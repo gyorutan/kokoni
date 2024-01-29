e1a484e87d7bfda2d8e2ab2b94b01c7de3abc08a
gyorutan <waou1998@gmail.com>
2024-01-29 20:10:02 +0900

    ui-fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="bg-slate-50 h-full flex flex-col gap-y-8 justify-center items-center">
-      <p className="text-2xl font-bold">JWT 인증 토큰 연습</p>
+      <p className="text-2xl font-bold flex gap-x-2">
+        <Image src={"/logo.png"} alt="logo" width={35} height={35} />
+        <span>kokoni</span>
+      </p>
       <div className="w-[200px] flex flex-col gap-y-4">
-        <Button asChild className="w-full">
+        <Button asChild className="w-full" variant={"blue"}>
           <Link href={"/auth/login"}>로그인</Link>
-        </Button>
-        <Button asChild variant={"outline"} className="w-full">
-          <Link href={"/auth/signup"}>가입</Link>
         </Button>
       </div>
     </div>
